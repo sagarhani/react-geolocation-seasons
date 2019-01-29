ca84952c47892a3702e05eb30c27084b95b1d605
sagarhani <sagarhani33@gmail.com>
2019-01-29 23:58:22 +0530

    Getting user geolocation

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
 const App = () => {
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => console.log(position),
+        (err) => console.log(err)
+    );
     return <div>Hi There!</div>;
 };
 
