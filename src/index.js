b65894cfcefd91d61b61187abd0d2385ecb51656
sagarhani <sagarhani33@gmail.com>
2019-01-30 23:12:06 +0530

    Rendering the latitude

--- src/index.js
+++ src/index.js
@@ -3,13 +3,34 @@ import ReactDOM from 'react-dom';
 
 import SeasonDisplay from './SeasonDisplay';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err)
-    );
-    return <div>Hi There!</div>;
-};
+class App extends React.Componenet {
+    constructor(props) {
+        super(props);
+
+        this.state = { lat: null };
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({ lat: position.coords.latitude, errorMessage: '' });
+            },
+            (err) => {
+                this.setState({ errorMessage: err.message });
+            }
+        );
+    }
+
+    render() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>
+        }
+
+        return <div>Loading...</div>
+    }
+}
 
 ReactDOM.render(
     <App />,
