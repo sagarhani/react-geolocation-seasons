70eacb44bec58f0fb9da0e69370c62db97b909f5
sagarhani <sagarhani33@gmail.com>
2019-01-31 23:49:08 +0530

    Updates

---
+++ precache-manifest.df9c08dace0eecc761ab2d2cbebcb246.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "0fbcc876a1d7761851f3",
+    "url": "/my-app/static/js/main.0fbcc876.chunk.js"
+  },
+  {
+    "revision": "3912e494b4cc3a5d3eda",
+    "url": "/my-app/static/js/1.3912e494.chunk.js"
+  },
+  {
+    "revision": "0fbcc876a1d7761851f3",
+    "url": "/my-app/static/css/main.8cfcabcc.chunk.css"
+  },
+  {
+    "revision": "1193b4132520923533e990c6edea05ed",
+    "url": "/my-app/index.html"
+  }
+];
